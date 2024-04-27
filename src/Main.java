656d23766b6a0b4efbede7fe3f0c8b290a49b530
Vitor Aurilandio <132232270+VitorAuri@users.noreply.github.com>
2024-04-27 16:24:44 -0300

    Delete src directory

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Teste teste = new Teste();
-        System.out.println(teste.soma(2,3));
-    }
-}
--- src/Teste.java
+++
@@ -1,5 +0,0 @@
-public class Teste {
-    public int soma(int a, int b){
-        return a + b;
-    }
-}
